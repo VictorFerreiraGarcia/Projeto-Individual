e2356ef245bc7b70bb1c1dd46a56fc08ea87cdb6
VictorFerreiraGarcia <victor.garcia@bandtec.com.br>
2021-05-31 18:58:26 -0300

    Alterações em praticamente tudo

--- Banco de Dados/projeto individual.sql
+++ Banco de Dados/projeto individual.sql
@@ -12,17 +12,19 @@ senha varchar(20)
 create table publicacao (
 id int primary key auto_increment,
 descricao varchar (1000),
+-- dataHora timestamp null default current_timestamp,
 fkUsuario int
 );
 
 create table filmes(
 id int primary key auto_increment,
 nome varchar (45),
-Diretor varchar (45),
+diretor varchar (45),
 dataLancamento date
 );
 
 alter table publicacao add foreign key (fkUsuario) references usuario(id);
+ -- alter table publicacao add foreign key (fkFilme) references filmes(id);
 
 create table notas (
 idNota int,
@@ -35,6 +37,13 @@ primary key (idNota, fkUsuario, fkFilme)
 alter table notas add foreign key (fkUsuario) references usuario(id);
 alter table notas add foreign key (fkFilme) references filmes(id);
 
+insert into filmes values
+(null,'300', 'Zack Snyder', '2007-03-09'),
+(null, '300: A Ascensão do Império ', 'Noam Murro', '2014-03-07');
+
+insert into usuario values
+(null, 'Victor Ferreira', 'Tito', 'victor.fgarcia@gmail.com', 'copmaster');
+
 select * from usuario;
 
 select * from publicacao;
@@ -43,10 +52,6 @@ select * from filmes;
 
 select * from notas;
 
-insert into filmes values
-(null,'300', 'Zack Snyder', '2007-03-09'),
-(null, '300: A Ascensão do Império ', 'Noam Murro', '2014-03-07');
-
-
-
+insert into publicacao values
+(null,'INCRÍVEL O FILME!!! Enredo e produção artística perfeitos, adorei',1);
 
